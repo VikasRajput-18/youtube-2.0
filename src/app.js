f68b6ba18ae8ce0ffa9019d152b5580304eda707
vikasrajput18 <vikasvikas98809@gmail.com>
2023-11-21 21:49:59 +0530

    routes and controller setup

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
+// Importing required modules
 import express from "express";
-import cors from "express";
+import cors from "express"; // This import statement seems incorrect. It should be `import cors from "cors";`
 import cookieParser from "cookie-parser";
 
+// Importing routes
+import userRoutes from "./routes/user.routes.js";
+
+// Creating an instance of Express
 export const app = express();
 
-app.use(cookieParser());
-app.use(express.json({ limit: "16kb" }));
+// Middleware setup
+app.use(cookieParser()); // Parse cookies
+app.use(express.json({ limit: "16kb" })); // Parse JSON requests with a limit of 16kb
 app.use(
   express.urlencoded({
-    extended: true,limit : "16kb"
+    extended: true,
+    limit: "16kb",
   })
 );
-app.use(express.static("public"))
+app.use(express.static("public")); // Serve static files from the "public" directory
+
+// CORS setup to allow cross-origin requests
 app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+
+// Route setup
+app.use("/api/v1/users", userRoutes); // Mount the user routes under the "/user" path
+
+// Note: It's important to handle errors, add more middleware, and configure other aspects of your application as needed.
--- src/index.js
+++ src/index.js
@@ -5,30 +5,6 @@ import connectToDB from "./db/index.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
-/*
-import express from "express";
-const app = express();
-
-const MONGO_URL = process.env.MONGODB_URI;
-
-(async () => {
-  try {
-    await mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
-    console.log("Connected Successfully");
-    app.on("error", (err) => {
-      console.log("Error : ", err);
-      throw err;
-    });
-
-    app.listen(PORT, () => {
-      console.log(`Server is listing on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error : ", error);
-    throw error;
-  }
-})();
-*/
 
 connectToDB()
   .then(() => {
