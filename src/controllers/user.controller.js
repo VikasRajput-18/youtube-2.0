d24bfa0e0700fc8ba473231b403ce91e5273ed1f
vikasrajput18 <vikasvikas98809@gmail.com>
2023-11-22 21:25:40 +0530

    add resgister controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,58 @@
+import User from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
+export const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, fullName, password } = req.body;
 
+  if (
+    [username, fullName, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 
+  const existedUser = await User.findOne({
+    $or: [{ email }, { username }],
+  });
 
-export const registerUser = asyncHandler(async(req, res)=>{
-    return res.status(200).json({
-        message : "ok"
-    })
-})
+  if (existedUser) {
+    throw new ApiError(409, "User already exist");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(409, "Avatar file is required ");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(409, "Avatar file is required ");
+  }
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || null,
+    password,
+  });
+
+  const userRegistered = await User.findById(user?._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!userRegistered) {
+    throw new Error(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, userRegistered, "User registered successfully"));
+});
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -20,5 +20,3 @@ export const uploadOnCloudinary = async (localFilePath) => {
     return null;
   }
 };
-
- 
